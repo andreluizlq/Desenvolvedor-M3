2aef063722b44a4c1f6d22e9ae4ce36c66007844
andreluizlq <andreluizlq1@gmail.com>
2024-01-11 14:14:54 -0300

    fix: Criação de filtros e listagem de produtos

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,138 @@
-import { Product } from "./Product";
+import { IProduct } from "./Product";
+const baseUrl = "http://localhost:5000/products";
+const checkColors = document.querySelectorAll(
+  '.colors-filters input[type="checkbox"]'
+);
+const checkSizes = document.querySelectorAll(
+  '.size-filters input[type="checkbox"]'
+);
+const checkPrices = document.querySelectorAll(
+  '.price-filters input[type="checkbox"]'
+);
+const moreProductButton = document.getElementById("more-product");
 
-const serverUrl = "http://localhost:5000";
+// Carregar produtos do server
+function loadProducts(callback: (products: IProduct[]) => void) {
+  const url: string = `${baseUrl}`;
 
-function main() {
-  console.log(serverUrl);
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Não foi possível obter os produtos da API");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      callback(data);
+      console.log("Produtos obtidos com sucesso");
+    })
+    .catch((error) => {
+      console.error("Erro ao obter os produtos:", error);
+    });
 }
 
-document.addEventListener("DOMContentLoaded", main);
+// Verificar se checkbox foi marcado
+function getCheckValues(checks: NodeListOf<Element>): string[] {
+  const values: string[] = [];
+  checks.forEach((check) => {
+    if ((check as HTMLInputElement).checked) {
+      values.push(check.getAttribute("id") || "");
+    }
+  });
+  return values;
+}
+
+// Filtrar produtos com base nas seleções do usuário
+function filterProducts(
+  products: IProduct[],
+  selectedColors: string[],
+  selectedSizes: string[],
+  selectedPrices: string[]
+): IProduct[] {
+  return products.filter((product) => {
+    const hasColor =
+      selectedColors.length === 0 || selectedColors.includes(product.color);
+    const hasSize =
+      selectedSizes.length === 0 ||
+      product.size.some((size) => selectedSizes.includes(size));
+    const hasPrices =
+      selectedPrices.length === 0 ||
+      selectedPrices.includes(getPriceRange(product.price));
+
+    return hasColor && hasSize && hasPrices;
+  });
+}
+
+// Faixa de preço com base no preço dos produtos
+function getPriceRange(price: number): string {
+  if (price >= 0 && price <= 50) return "0-50";
+  if (price > 50 && price <= 150) return "51-150";
+  if (price > 150 && price <= 300) return "151-300";
+  if (price > 300 && price <= 500) return "301-500";
+  if (price >= 500) return "500-more";
+  return;
+}
+
+// Criando elemento HTML
+function createProductHTML(product: IProduct): HTMLElement {
+  const productHTML: HTMLElement = document.createElement("li");
+  productHTML.innerHTML = `
+  <img class="card-photo" src="${product.image}" alt="${product.name}" />
+  <h4 class="card-name">${product.name}</h2>
+    <div class="card-price">
+      <p>R$ ${product.price}</p>
+      <p>até ${product.parcelamento.join("x de R$")}</p>
+    </div>
+    <div class="card-content-size">
+      <div class="card-size">${product.size.join(", ")}</div>
+    </div>
+    <a href="#" class="card-buy">Comprar</a>
+  `;
+  return productHTML;
+}
+
+// Atualização de filtragem de produtos
+function updateProductList(orderOption: any) {
+  const selectedColors = getCheckValues(checkColors);
+  const selectedSizes = getCheckValues(checkSizes);
+  const selectedPrices = getCheckValues(checkPrices);
+
+  loadProducts((products) => {
+    const productList = document.getElementById("products");
+    if (productList) {
+      // Limpando productList
+      productList.innerHTML = "";
+      // Filtragem de produtos selecionados
+      const filteredProducts = filterProducts(
+        products,
+        selectedColors,
+        selectedSizes,
+        selectedPrices
+      );
+      if (filteredProducts.length !== 0) {
+        // Criação e renderização da lista sem filtros
+        products.forEach((product: IProduct) => {
+          const productDiv: HTMLElement = createProductHTML(product);
+          productList.appendChild(productDiv);
+        });
+        // Limpando productList
+        productList.innerHTML = "";
+        // Criação e renderização da lista filtrada
+        filteredProducts.forEach((product: IProduct) => {
+          const productDiv: HTMLElement = createProductHTML(product);
+          productList.appendChild(productDiv);
+        });
+      } else {
+        // Mensagem caso nenhum produto atenda aos filtros selecionados
+        const alertMessage: HTMLElement = document.createElement("li");
+        alertMessage.classList.add("alert-message");
+        alertMessage.innerText = "Nenhum produto encontrado.";
+        productList.appendChild(alertMessage);
+        moreProductButton.style.display = "none";
+      }
+    }
+  });
+}
+
+// Atualização de lista de produtos ao inicializar
+document.addEventListener("DOMContentLoaded", updateProductList);
